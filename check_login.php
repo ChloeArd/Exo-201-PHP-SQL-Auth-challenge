dfa48c330e633b76a64bff95e60eefa4686216b4
ChloeArd <chloe.ardoise@gmail.com>
2021-03-04 13:46:47 +0100

    Exercice 201

--- check_login.php
+++ check_login.php
@@ -1,2 +1,26 @@
 <?php
 //Check if credentials are valid
+include "./lib/functions.php";
+require "./Classes/DB.php";
+
+if (issetPostParams('username', 'password')) {
+    $bdd = DB::getInstance();
+
+    $username = sanitize($_POST['username']);
+    $password = sanitize($_POST['password']);
+
+    $stmt = $bdd->prepare("SELECT * FROM user WHERE username = '$username' AND password = '$password'");
+
+    $result = $stmt->execute();
+
+    foreach ($stmt->fetchAll() as $user) {
+        echo "Username = " . $user['username'] . ", password = " . $user['password'];
+        session_start ();
+        $_SESSION['username'] = $username;
+        $_SESSION['password'] = $password;
+
+        header("Location: bienvenue.php");
+    }
+}
+echo '<body onLoad="alert(\'Membre non reconnu !\')">';
+echo '<meta http-equiv="refresh" content="0;URL=login.php">';
